e7ae1d382b6dd64f58fc33e0d7878e0ffec9438c
Josh Hsu <88902013+JHsu7@users.noreply.github.com>
2022-03-08 12:54:36 -0600

    3/8

---
+++ Voluntold/app/src/main/java/com/example/voluntold/OrganizationInformation.java
@@ -0,0 +1,14 @@
+package com.example.voluntold;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrganizationInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.organization_info_screen);
+    }
+}
---
+++ Voluntold/app/src/main/java/com/example/voluntold/OrganizationSignUp1.java
@@ -0,0 +1,14 @@
+package com.example.voluntold;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrganizationSignUp1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.organization_signup_1);
+    }
+}
