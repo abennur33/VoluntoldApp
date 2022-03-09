d823c888aa51cd9e21ff58038c5de9748552150e
Josh Hsu <88902013+JHsu7@users.noreply.github.com>
2022-03-09 12:18:00 -0600

    3/9

---
+++ Voluntold/app/src/main/java/com/example/voluntold/OrganizationSignUp2.java
@@ -0,0 +1,14 @@
+package com.example.voluntold;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrganizationSignUp2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.organization_signup_2);
+    }
+}
