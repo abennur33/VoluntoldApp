9914b9bc50be881bcfe16479cccffb68a8c37989
Aadit B <62301974+GhoulDome@users.noreply.github.com>
2022-03-09 12:16:34 -0600

    dashboards made

---
+++ Voluntold/app/src/main/java/com/example/voluntold/OrgSide/OrgDashboard.java
@@ -0,0 +1,16 @@
+package com.example.voluntold.OrgSide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.voluntold.R;
+
+public class OrgDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_org_dashboard);
+    }
+}
