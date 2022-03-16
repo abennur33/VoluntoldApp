f49c697086a806b1b59bb2dcae587c125f2966c1
Josh Hsu <88902013+JHsu7@users.noreply.github.com>
2022-03-16 12:46:25 -0500

    post stuff

---
+++ Voluntold/app/src/main/java/com/example/voluntold/OpportunityPost.java
@@ -0,0 +1,14 @@
+package com.example.voluntold;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OpportunityPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.opportunity_post);
+    }
+}
