d5d93c4ca1813468240d147539b0792a418e4d94
Aadit B <62301974+GhoulDome@users.noreply.github.com>
2022-03-16 12:43:54 -0500

    orgpost

--- Voluntold/app/src/main/java/com/example/voluntold/OrgCreateOpportunityPost.java
+++ Voluntold/app/src/main/java/com/example/voluntold/OrgCreateOpportunityPost.java
@@ -3,12 +3,38 @@ package com.example.voluntold;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class OrgCreateOpportunityPost extends AppCompatActivity {
+
+    private String title, body;
+    private int month, day, year;
+    private EditText titleET, bodyET, monthET, dayET, yearET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.org_create_opportunity_post);
+
+        titleET = findViewById(R.id.titleET);
+        bodyET = findViewById(R.id.bodyET);
+        monthET = findViewById(R.id.monthET);
+        dayET = findViewById(R.id.dayET);
+        yearET = findViewById(R.id.yearET);
+    }
+
+    public void Post(View v) {
+        title = titleET.getText().toString();
+        body = bodyET.getText().toString();
+        month = Integer.parseInt(monthET.getText().toString());
+        day = Integer.parseInt(dayET.getText().toString());
+        year = Integer.parseInt(yearET.getText().toString());
+
+        String uid = LogIn.firebaseHelper.getmAuth().getUid();
+
+        OrgPost orgPost = new OrgPost(uid, title, month, day, year, body);
+
+        LogIn.firebaseHelper.addPost(orgPost);
     }
 }
