9b2cea8cb2b2a4a4b0fac95abefbbdc09190778b
Josh Hsu <88902013+JHsu7@users.noreply.github.com>
2022-03-17 12:09:51 -0500

    updated manifest

---
+++ Voluntold/app/src/main/java/com/example/voluntold/OpportunitiesView.java
@@ -0,0 +1,14 @@
+package com.example.voluntold;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OpportunitiesView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.opportunities_view);
+    }
+}
