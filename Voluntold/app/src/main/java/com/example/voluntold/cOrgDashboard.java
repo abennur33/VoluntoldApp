f3fcade72b8418330d4fc2fad0f497b1020af54b
luisxuu <88901969+luisxuu@users.noreply.github.com>
2022-03-28 12:36:12 -0500

    sdfASDFSDfSDf

--- Voluntold/app/src/main/java/com/example/voluntold/cOrgDashboard.java
+++ Voluntold/app/src/main/java/com/example/voluntold/cOrgDashboard.java
@@ -5,17 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
+
 public class cOrgDashboard extends AppCompatActivity {
+
+    private ArrayList<OrgPost> postList;
 
     private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_org_dashboard);
+        setContentView(R.layout.opportunities_view);
 
+        // NEED FUNCTION TO GET ARRAYLIST OF ALL POSTS WHEN DONE
+        postList = aMainActivity.firebaseHelper.();
+        Intent intent = getIntent();
+
+        ArrayAdapter<OrgPost> listAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, postList);
+        // may change simple_list_item_1 to custom ListView layout
+
+        ListView listView = (ListView) findViewById(R.id.postView);
+        listView.setAdapter(listAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(OpportunitiesView.this, OpportunityPost.class);
+
+                // Sends the specific object at index i to the Edit activity
+                // In this case, it is sending the particular WishListItem object
+                intent.putExtra("ITEM_TO_EDIT", postList.get(i));
+                startActivity(intent);
+            }
         mAuth = FirebaseAuth.getInstance();
     }
 
