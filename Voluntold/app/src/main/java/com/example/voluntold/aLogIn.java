d0f8b39733a91f72fbba5f6260984291a82df06f
abhinavbhashyam <84284109+abhinavbhashyam@users.noreply.github.com>
2022-04-07 12:55:33 -0500

    Added loading screen

--- Voluntold/app/src/main/java/com/example/voluntold/aLogIn.java
+++ Voluntold/app/src/main/java/com/example/voluntold/aLogIn.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -51,6 +52,15 @@ public class aLogIn extends AppCompatActivity {
     }
 
     public void takeToScreenUponLogIn() {
+        LoadingDialog loadingDialog = new LoadingDialog(aLogIn.this);
+        loadingDialog.startLoadingDialog();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                loadingDialog.dismissDialog();
+            }
+        }, 2500);
         Intent intent = new Intent(aLogIn.this, aIntermediateLogInScreen.class);
         startActivity(intent);
     }
