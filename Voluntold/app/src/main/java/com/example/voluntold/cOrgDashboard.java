cee2af1b27f3a3c934f7c39049012cb7be094637
Josh Hsu <88902013+JHsu7@users.noreply.github.com>
2022-04-07 12:42:19 -0500

    adsfdas

--- Voluntold/app/src/main/java/com/example/voluntold/cOrgDashboard.java
+++ Voluntold/app/src/main/java/com/example/voluntold/cOrgDashboard.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
