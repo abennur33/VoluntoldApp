0ce43c6daf5de287fa988b9b4bc6624772372d78
Josh Hsu <88902013+JHsu7@users.noreply.github.com>
2022-04-20 12:53:10 -0500

    afdda

---
+++ Voluntold/app/src/main/java/com/example/voluntold/cViewVolunteers.java
@@ -0,0 +1,14 @@
+package com.example.voluntold;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cViewVolunteers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.org_view_volunteers);
+    }
+}
